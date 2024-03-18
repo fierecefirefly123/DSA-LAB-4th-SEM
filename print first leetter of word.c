483bd28b911307bfcf6c7d50d2ec340cc5ddac3a
fierecefirefly123 <120125342+fierecefirefly123@users.noreply.github.com>
2024-03-18 11:17:01 +0530

    Add files via upload

---
+++ print first leetter of word.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    char a[10]={"vaishak"};
+    int *p;
+    p=&a;
+    printf("%d",a);
+    printf("\n");
+    printf("%s",a);
+    printf("\n");
+    printf("%c",a[0]);
+    printf("\n");
+}
